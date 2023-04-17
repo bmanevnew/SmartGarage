0e5a3471f4039c39c0ce88647a1fd08d7ecefedc
Boris Manev <boris.manev.a44@learn.telerikacademy.com>
2023-04-17 17:04:48 +0300

    Adding EmailSenderServiceTests.java

--- src/test/java/com/company/web/smart_garage/services/EmailSenderServiceTests.java
+++ src/test/java/com/company/web/smart_garage/services/EmailSenderServiceTests.java
@@ -1,4 +1,64 @@
 package com.company.web.smart_garage.services;
 
+import com.company.web.smart_garage.services.impl.EmailSenderServiceImpl;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Multipart;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
 public class EmailSenderServiceTests {
+    @Mock
+    private JavaMailSender mailSender;
+
+    @InjectMocks
+    private EmailSenderServiceImpl emailSenderService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void sendEmail_Success() {
+        String toEmail = "test@example.com";
+        String subject = "Test Subject";
+        String body = "Test Body";
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("smartGarageRepairs@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        emailSenderService.sendEmail(toEmail, subject, body);
+
+        verify(mailSender, Mockito.times(1)).send(Mockito.eq(message));
+    }
+
+    @Test
+    public void sendEmailWithAttachment_Success() throws MessagingException, IOException {
+        JavaMailSender mailSender = mock(JavaMailSender.class);
+        MimeMessage mimeMessage = mock(MimeMessage.class);
+        when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
+
+        EmailSenderServiceImpl emailSenderService = new EmailSenderServiceImpl(mailSender);
+        emailSenderService.sendEmailWithAttachment("test@example.com", "Test Email", "Test Body", "/path/to/file.txt");
+
+        verify(mailSender, times(1)).send(mimeMessage);
+        verify(mimeMessage, times(1)).setContent(any(Multipart.class));
+    }
 }
