0bd97d3cc83d1adaea00a97c33da127d166cdd09
GeorgiMihaylov <georgi.mihaylov.a44@learn.telerikacademy.com>
2023-04-24 19:29:36 +0300

    Adding visit.html and delete functionality

--- src/main/java/com/company/web/smart_garage/controllers/mvc/VisitsMvcController.java
+++ src/main/java/com/company/web/smart_garage/controllers/mvc/VisitsMvcController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -112,5 +113,16 @@ public class VisitsMvcController {
         }
         viewModel.addAttribute("visits", visits);
         return "allVisits";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable long id) {
+        try {
+            visitService.delete(id);
+        } catch (InvalidParamException e) {
+            throw new EntityNotFoundException("Vehicle", id);
+        }
+        return "redirect:/vehicles";
     }
 }
